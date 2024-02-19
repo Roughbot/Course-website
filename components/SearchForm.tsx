78d079dbf01dfd0e670257e2f1128204e312302f
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-02-19 20:52:35 +0530

    hero image

---
+++ components/SearchForm.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import React from "react";
+
+const SearchForm = () => {
+  return (
+    <form>
+      <label>
+        <Image />
+      </label>
+    </form>
+  );
+};
+
+export default SearchForm;
